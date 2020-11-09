12808a641d8c653ed165d604256c036e7d68a942
Le Tuan <tuancules24@gmail.com>
2020-11-09 14:45:35 +0700

    add form

---
+++ admin/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="vi">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="shortcut icon" type="image/png" href="">
+    <!-- Bootstrap 4 CSS -->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <!-- Thu vien animate -->
+    <link rel="stylesheet" type="text/css" href="css/animate.css">
+    <!-- Khai bao fontawesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css">
+    <!-- Thu vien jquery -->
+    <script src="js/jquery-3.5.0.min.js"></script>
+    <!-- Bootstrap 4 js -->
+    <script src="js/bootstrap.min.js"></script>
+    <!-- Thu vien wow js -->
+    <script src="js/wow.min.js"></script>
+    <title>Fantastic 3 Cineplex</title>
+  </head>
+  <body>
+            <h1>Trang trủ admin</h1>       
+  
+    
+  </body>
+</html>
